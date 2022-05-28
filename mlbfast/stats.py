f56812f9c99363346a79541e7895029027e971af
armstjc <70788681+armstjc@users.noreply.github.com>
2022-05-28 11:27:34 -0400

    v0.2.1: Added season pitching stats

--- mlbfast/stats.py
+++ mlbfast/stats.py
@@ -124,3 +124,80 @@ def getSeasonHittingStats(playerID=0,gameType="R",season=0):
 			print(f'No results found for the provided playerID. \nTry a diffrient search for better results.')
 		
 		return main_df
+
+
+def getSeasonPitchingStats(playerID=0,gameType="R",season=0):
+	'''
+	Retrives the player info for an MLB player, given a proper MLBAM ID
+
+	Args:
+	
+	playerID (int):
+		Required paramater. If no playerID is provided, the function wil not work.
+	'''
+	pullCopyrightInfo()
+	#p_df = pd.DataFrame()
+	main_df = pd.DataFrame()
+	
+	searchURL = "http://lookup-service-prod.mlb.com/json/named.sport_pitching_tm.bam?league_list_id='mlb'&"
+
+	if len(gameType) >1:
+		print('Check your input for seasonType. Searching for regular season stats instead.')
+		gameType = "R"
+		searchURL = searchURL  + f'game_type=\'{gameType}\'&'
+	else:
+		pass
+
+	#if gameType == "R" or gameType == "S" or gameType == "E" or gameType == "A" orgameType == "D" orgameType == "F" orgameType == "L" orgameType == "W":
+	#	searchURL = searchURL  + f'game_type=\'{gameType}\'&'
+	#else:
+	#	print('Check your input for seasonType. Searching for regular season stats instead.')
+	#	gameType = "R"
+	#	searchURL = searchURL  + f'game_type=\'{gameType}\'&'
+	searchURL = searchURL  + f'game_type=\'{gameType}\'&'
+
+	now = datetime.now()
+	if season < 1860:
+		print('Please input a proper year. The search will continue with the current year instead.')
+		season = int(now.year)
+		searchURL = searchURL  + f'season=\'{season}\'&'
+	elif int(now.year) < season:
+		print('Please input a proper year. The search will continue with the current year instead.')
+		season = int(now.year)
+		searchURL = searchURL  + f'season=\'{season}\'&'
+	else:
+		searchURL = searchURL  + f'season=\'{season}\'&'
+
+
+
+	if playerID < 1:
+		print('You must provide a playerID. Without a proper playerID, this function will not work.')
+		return None
+	else:
+		searchURL= searchURL + f"player_id=\'{playerID}\'"
+		
+		
+		#searchURL = urllib.parse.quote_plus(str(searchURL))
+		resp = download(searchURL)
+
+		print(searchURL)
+		resp_str = str(resp, 'UTF-8')
+		print(resp_str)
+
+		resp_json = json.loads(resp_str)
+		try:
+			result_count = int(resp_json['sport_hitting_tm']['queryResults']['totalSize'])
+		except:
+			result_count = 0
+
+		if result_count > 0:
+			#print(resp_json['player_teams']['queryResults']['row'])
+
+			print(f'{result_count} statlines found,\nParsing results into a dataframe.')
+			#players = resp_json['search_player_all']['queryResults']['row']
+			main_df = json_normalize(resp_json['sport_hitting_tm']['queryResults']['row']) 
+			print('Done')
+		else:
+			print(f'No results found for the provided playerID. \nTry a diffrient search for better results.')
+		
+		return main_df
