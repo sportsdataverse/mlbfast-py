dee15206702895df6e60af787d7b2093c258df54
armstjc <70788681+armstjc@users.noreply.github.com>
2022-05-28 11:45:39 -0400

    v0.2.2 Added career stats.

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ import pandas as pd
 
 #pullCopyrightInfo()
 #print('Improper input for the isActive input. \nIf you want active players, set isActive to "Y" or "Yes". \nIf you want inactive players, set isActive to "N" or "No".\n\nIn the meantime, your search will search for all players in MLB history.')
-df = getSeasonHittingStats(592789,"R",2017)
+df = getProjectedHittingStats(493316,"R")
 print(df)
 #df.to_csv('test.csv')
