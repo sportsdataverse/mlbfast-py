fc415fa6693b20e959220b81be63f40f0ae2b2f5
armstjc <70788681+armstjc@users.noreply.github.com>
2022-05-28 13:30:28 -0400

    v0.2.3: Added Team functions

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from mlbfast.players import *
 from mlbfast.stats import *
+from mlbfast.teams import *
 import pandas as pd
 
 
 #pullCopyrightInfo()
 #print('Improper input for the isActive input. \nIf you want active players, set isActive to "Y" or "Yes". \nIf you want inactive players, set isActive to "N" or "No".\n\nIn the meantime, your search will search for all players in MLB history.')
-df = getProjectedHittingStats(493316,"R")
+df = getAllTimeRoster(121,2016,2017)
 print(df)
-#df.to_csv('test.csv')
+df.to_csv('test.csv')
